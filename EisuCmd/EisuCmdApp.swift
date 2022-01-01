2784b0040fd0bb4067020270d7d0fa8326cf2821
Hideo Matsumoto <pekeq.9@gmail.com>
2022-01-01 21:57:30 +0900

    Initial Commit

---
+++ EisuCmd/EisuCmdApp.swift
@@ -0,0 +1,17 @@
+//
+//  EisuCmdApp.swift
+//  EisuCmd
+//
+//  Created by hideo-m on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct EisuCmdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
